da4085fd1692dd9849a431dd0fd119b18a7e6a41
Leonardo Velazquez <leo.vel330@gmail.com>
2022-10-08 14:12:38 -0500

    Creación de borrador del funcionamiento

--- AccesoDatos/ADiniciarSesion.cs
+++ AccesoDatos/ADiniciarSesion.cs
@@ -5,14 +5,19 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
 using System.Windows.Forms;
 
 namespace AccesoDatos
 {
     public class ADiniciarSesion
     {
-        public void IniciarSesion(string usuario, string pass)
+        Base b = new Base("localhost","root","","agenciaautomotriz");
+        public DataSet mostrar(string usuario, string pass)
+        {
+            return b.Obtener(string.Format("select nombre, pass from usuario where nombre = '{0}' and pass = '{1}'", usuario, pass), "usuario");
+        }
+        
+        /*public void IniciarSesion(string usuario, string pass)
         {
             MySqlConnection con = new MySqlConnection("server=localhost; Database=agenciaautomotriz; User Id=root; password=");
             try
@@ -33,6 +38,6 @@ namespace AccesoDatos
             }
             else
                 MessageBox.Show("El usuario no existe");
-        }
+        }*/
     }
 }
--- PresentacionPermisosUsuarios/FrmInicioSesion.cs
+++ PresentacionPermisosUsuarios/FrmInicioSesion.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AccesoDatos;
+using Manejador;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,14 +14,31 @@ namespace PresentacionPermisosUsuarios
 {
     public partial class FrmInicioSesion : Form
     {
+        ADiniciarSesion adis;
         public FrmInicioSesion()
         {
             InitializeComponent();
+            adis = new ADiniciarSesion();
         }
         string usuario, contra;
         private void btnIniciar_Click(object sender, EventArgs e)
         {
-            //usuario=
+            var ds = adis.mostrar(txtUsuario.Text,txtPass.Text);
+            var dt = new DataTable();
+            dt = ds.Tables[0];
+            try
+            {
+                usuario = dt.Rows[0]["nombre"].ToString();
+                contra = dt.Rows[0]["pass"].ToString();
+                if (usuario == txtUsuario.Text && contra == txtPass.Text)
+                {
+                    MessageBox.Show("Bienvenido owo");
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Uno o más campos incorrectos");
+            }
         }
     }
 }
